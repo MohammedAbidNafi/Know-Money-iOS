c7c0ceb84a94620f6491d4ed0f69574e1a4cc8bc
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-03-19 22:42:54 +0530

    Intial Commit

---
+++ Know-Money/KnowMoneyApp.swift
@@ -0,0 +1,46 @@
+//
+//  ExpensoApp.swift
+//  Expenso
+//
+//  Created by Sameer Nawaz on 31/01/21.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct KnowMoneyApp: App {
+    
+    init() {
+        self.setDefaultPreferences()
+    }
+    
+    private func setDefaultPreferences() {
+        let currency = UserDefaults.standard.string(forKey: UD_EXPENSE_CURRENCY)
+        if currency == nil {
+            UserDefaults.standard.set("₹", forKey: UD_EXPENSE_CURRENCY)
+        }
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            if UserDefaults.standard.bool(forKey: UD_USE_BIOMETRIC) {
+                AuthenticateView(viewModel: AuthenticationViewModel())
+                    .environment(\.managedObjectContext, persistentContainer.viewContext)
+            } else {
+                ExpenseView()
+                    .environment(\.managedObjectContext, persistentContainer.viewContext)
+            }
+        }
+    }
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Know Money")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+}
